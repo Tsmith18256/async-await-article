fbd8ed4213ec3eeac4469c501d01da880ab0e750
Tyler Smith <tyler.smith@pteraforce.com>
2016-11-11 13:03:24 -0800

    Convert callbacks to promises

--- app/task.controller.js
+++ app/task.controller.js
@@ -5,57 +5,54 @@ let Task = require('mongoose').model('Task');
 exports.create = (req, res) => {
   let task = new Task(req.body);
 
-  task.save((err, task) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else {
-      res.json({ task: task });
-    }
-  });
+  task.save().then(
+    task => res.json({ task: task }),
+    err => res.status(500).json({ error: err })
+  );
 };
 
 exports.delete = (req, res) => {
-  Task.findByIdAndRemove(req.params.taskId, (err, task) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else if (!task) {
-      res.status(400).json({ error: 'No task with the given ID' });
-    } else {
-      res.json({ task: task });
-    }
-  });
+  Task.findByIdAndRemove(req.params.taskId).exec().then(
+    task => {
+      if (!task) {
+        res.status(400).json({ error: 'No task with the given ID' });
+      } else {
+        res.json({ task: task });
+      }
+    },
+    err => res.status(500).json({ error: err })
+  );
 };
 
 exports.read = (req, res) => {
-  Task.findById(req.params.taskId, (err, task) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else if (!task) {
-      res.status(400).json({ error: 'No task with the given ID' });
-    } else {
-      res.json({ task: task });
-    }
-  });
+  Task.findById(req.params.taskId).exec().then(
+    task => {
+      if (!task) {
+        res.status(400).json({ error: 'No task with the given ID' });
+      } else {
+        res.json({ task: task });
+      }
+    },
+    err => res.status(500).json({ error: err })
+  );
 };
 
 exports.readAll = (req, res) => {
-  Task.find({}, (err, tasks) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else {
-      res.json({ tasks: tasks });
-    }
-  });
+  Task.find({}).exec().then(
+    tasks => res.json({ tasks: tasks }),
+    err => res.status(500).json({ error: err })
+  );
 };
 
 exports.update = (req, res) => {
-  Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true }, (err, task) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else if (!task) {
-      res.status(400).json({ error: 'No task with the given ID' });
-    } else {
-      res.json({ task: task });
-    }
-  });
+  Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true }).exec().then(
+    task => {
+      if (!task) {
+        res.status(400).json({ error: 'No task with the given ID' });
+      } else {
+        res.json({ task: task });
+      }
+    },
+    err => res.status(500).json({ error: err })
+  );
 };
--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,8 @@
 let mongoose = require('mongoose');
 
 module.exports = () => {
+  mongoose.Promise = global.Promise;
+
   let db = mongoose.connect('mongodb://localhost/async-await-article');
 
   require('../app/task.model');
