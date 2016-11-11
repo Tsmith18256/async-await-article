7a258de0612312cfefba2c79975583704ffbfcd0
Tyler Smith <tyler.smith@pteraforce.com>
2016-11-11 13:27:59 -0800

    Convert promises to async await

--- app/task.controller.js
+++ app/task.controller.js
@@ -2,57 +2,64 @@
 
 let Task = require('mongoose').model('Task');
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
   let task = new Task(req.body);
 
-  task.save().then(
-    task => res.json({ task: task }),
-    err => res.status(500).json({ error: err })
-  );
+  try {
+    let t = await task.save();
+    res.json({ task: t });
+  } catch(err) {
+    res.status(500).json({ error: err });
+  }
 };
 
-exports.delete = (req, res) => {
-  Task.findByIdAndRemove(req.params.taskId).exec().then(
-    task => {
-      if (!task) {
-        res.status(400).json({ error: 'No task with the given ID' });
-      } else {
-        res.json({ task: task });
-      }
-    },
+exports.delete = async (req, res) => {
+  let task = await Task.findByIdAndRemove(req.params.taskId);
+
+  try {
+    if (!task) {
+      res.status(400).json({ error: 'No task with the given ID' });
+    } else {
+      res.json({ task: task });
+    }
+  } catch(err) {
     err => res.status(500).json({ error: err })
-  );
+  }
 };
 
-exports.read = (req, res) => {
-  Task.findById(req.params.taskId).exec().then(
-    task => {
-      if (!task) {
-        res.status(400).json({ error: 'No task with the given ID' });
-      } else {
-        res.json({ task: task });
-      }
-    },
-    err => res.status(500).json({ error: err })
-  );
+exports.read = async (req, res) => {
+  try {
+    let task = await Task.findById(req.params.taskId).exec();
+
+    if (!task) {
+      res.status(400).json({ error: 'No task with the given ID' });
+    } else {
+      res.json({ task: task });
+    }
+  } catch(err) {
+    res.status(500).json({ error: err });
+  }
 };
 
-exports.readAll = (req, res) => {
-  Task.find({}).exec().then(
-    tasks => res.json({ tasks: tasks }),
-    err => res.status(500).json({ error: err })
-  );
+exports.readAll = async (req, res) => {
+  try {
+    let tasks = await Task.find({}).exec();
+    res.json({ tasks: tasks });
+  } catch(err) {
+    res.status(500).json({ error: err })
+  }
 };
 
-exports.update = (req, res) => {
-  Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true }).exec().then(
-    task => {
-      if (!task) {
-        res.status(400).json({ error: 'No task with the given ID' });
-      } else {
-        res.json({ task: task });
-      }
-    },
+exports.update = async (req, res) => {
+  try {
+    let task = await Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true });
+
+    if (!task) {
+      res.status(400).json({ error: 'No task with the given ID' });
+    } else {
+      res.json({ task: task });
+    }
+  } catch(err) {
     err => res.status(500).json({ error: err })
-  );
+  }
 };
